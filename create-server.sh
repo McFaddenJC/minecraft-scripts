b40e981760f63aa327f294e538a4eeccae606431
Joseph <mcfaddenjc@minecraft2.home>
2019-10-09 20:32:13 -0400

    Added checks for server type and gamemode

--- create-server.sh
+++ create-server.sh
@@ -1,83 +1,102 @@
 #!/bin/bash
 
 # Get server type and location
-if [ $# -lt 3 ]; then
-  echo ""
-  echo "Usage: $0 <release/snapshot> <server_name> <port#>"
+USAGE="Usage: $0 <release/snapshot> <server_name> <port#> <gamemode>"
+if [ $# -lt 4 ]; then
+  echo "Invalid number of arguments given"
+  echo "$USAGE"
   echo ""
   exit 1;
+fi
+  
+# Set any environment variables here
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR=$( sed 's|/minecraft-scripts||g' <<< $SCRIPTS_DIR )
+SERVER_TYPE=$1
+SERVER_NAME=$2
+SERVER_PORT=$3
+GAME_MODE=$4
+
+# Make sure SERVER_TYPE is valid
+if [ $SERVER_TYPE != "snapshot" || $SERVER_TYPE != "release" ]; then
+  echo "Invalid server type given"
+  echo "$USAGE"
+  echo ""
+  exit 1;
+fi
+
+# Make sure the game mode is valid
+if [ $GAME_MODE != "survival" || $GAME_MODE != "adventure" || $GAME_MODE != "creative" ]; then
+  echo "Invalid game mode!"
+  echo "$USAGE"
+  echo ""
+  exit 1;
+fi
+
+# Add server to the list of servers to start/stop
+$SCRIPTS_DIR/add-server.sh $SERVER_NAME 
+
+# Check to see if the folder/directory exists
+if [ -e "$BASE_DIR/$SERVER_NAME" ]; then
+  echo "The server exists"
+  # Copy in the server icon to make sure it's up to date
+  echo "Copying server-icon to server"
+  cp $SCRIPTS_DIR/server-icon.png $BASE_DIR/$SERVER_NAME
+  echo "Downloading latest $SERVER_TYPE server"
+  $SCRIPTS_DIR/get-server.sh $SERVER_TYPE $SERVER_NAME
 else
-  # Set any environment variables here
-  SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-  BASE_DIR=$( sed 's|/minecraft-scripts||g' <<< $SCRIPTS_DIR )
-  SERVER_TYPE=$1
-  SERVER_NAME=$2
-  SERVER_PORT=$3
+  echo "The server does not exist yet"
+  # Create the directory and copy in the server icon
+  echo "Creating server structure"
+  mkdir -p "$BASE_DIR/$SERVER_NAME"
+  echo "Copying server-icon to server"
+  cp "$SCRIPTS_DIR/server-icon.png" "$BASE_DIR/$SERVER_NAME"
 
-  # Add server to the list of servers to start/stop
-  $SCRIPTS_DIR/add-server.sh $SERVER_NAME 
+  # Download the latest release/snapshot to the server structure
+  echo "Downloading latest $SERVER_TYPE server"
+  $SCRIPTS_DIR/get-server.sh $SERVER_TYPE $SERVER_NAME
 
-  # Check to see if the folder/directory exists
-  if [ -e "$BASE_DIR/$SERVER_NAME" ]; then
-    echo "The server exists"
-    # Copy in the server icon to make sure it's up to date
-    echo "Copying server-icon to server"
-    cp $SCRIPTS_DIR/server-icon.png $BASE_DIR/$SERVER_NAME
-    echo "Downloading latest $SERVER_TYPE server"
-    $SCRIPTS_DIR/get-server.sh $SERVER_TYPE $SERVER_NAME
-  else
-    echo "The server does not exist yet"
-    # Create the directory and copy in the server icon
-    echo "Creating server structure"
-    mkdir -p "$BASE_DIR/$SERVER_NAME"
-    echo "Copying server-icon to server"
-    cp "$SCRIPTS_DIR/server-icon.png" "$BASE_DIR/$SERVER_NAME"
+  # Start the new server to generate configuration files
+  echo "Navigating to the new server"
+  cd "$BASE_DIR/$SERVER_NAME"
 
-    # Download the latest release/snapshot to the server structure
-    echo "Downloading latest $SERVER_TYPE server"
-    $SCRIPTS_DIR/get-server.sh $SERVER_TYPE $SERVER_NAME
-
-    # Start the new server to generate configuration files
-    echo "Navigating to the new server"
-    cd "$BASE_DIR/$SERVER_NAME"
-
-    # Get the latest version
-    CURRENT_SERVER=$( cat "$BASE_DIR/$SERVER_NAME/current_server" )
+  # Get the latest version
+  CURRENT_SERVER=$( cat "$BASE_DIR/$SERVER_NAME/current_server" )
+  
+  # Create the launch script using the latest release
+  #   Remove the current launch script first
+  if [ -e "$BASE_DIR/$SERVER_NAME/launch.sh" ]; then
+    rm "$BASE_DIR/$SERVER_NAME/launch.sh"
+  fi
     
-    # Create the launch script using the latest release
-    #   Remove the current launch script first
-    if [ -e "$BASE_DIR/$SERVER_NAME/launch.sh" ]; then
-      rm "$BASE_DIR/$SERVER_NAME/launch.sh"
-    fi
-    
-    # Create new launch.sh file
-    echo "#!/bin/bash" >> "$BASE_DIR/$SERVER_NAME/launch.sh"
-    echo "$BASE_DIR/java/bin/java -server -Xmx3096M -Xms3096M -jar minecraft_server.$CURRENT_SERVER.jar" >> "$BASE_DIR/$SERVER_NAME/launch.sh"
-    chmod +x "$BASE_DIR/$SERVER_NAME/launch.sh"
+  # Create new launch.sh file
+  echo "#!/bin/bash" >> "$BASE_DIR/$SERVER_NAME/launch.sh"
+  echo "$BASE_DIR/java/bin/java -server -Xmx3096M -Xms3096M -jar minecraft_server.$CURRENT_SERVER.jar" >> "$BASE_DIR/$SERVER_NAME/launch.sh"
+  chmod +x "$BASE_DIR/$SERVER_NAME/launch.sh"
 
-    if [ ! -e "$BASE_DIR/$SERVER_NAME/eula.txt" ]; then
-      echo "Starting server to generate config files"
-      $BASE_DIR/$SERVER_NAME/launch.sh
+  if [ ! -e "$BASE_DIR/$SERVER_NAME/eula.txt" ]; then
+    echo "Starting server to generate config files"
+    $BASE_DIR/$SERVER_NAME/launch.sh
       
-      # Set config file properties via user prompt
-      sed -i 's/eula=false/eula=true/g' "$BASE_DIR/$SERVER_NAME/eula.txt"
-      sed -i 's/difficulty=easy/difficulty=normal/g' "$BASE_DIR/$SERVER_NAME/server.properties"
-      sed -i 's/pvp=true/pvp=false/g' "$BASE_DIR/$SERVER_NAME/server.properties"
-      sed -i "s/server-port=25565/server-port=$SERVER_PORT/g" "$BASE_DIR/$SERVER_NAME/server.properties"
-      sed -i 's/max-players=20/max-players=10/g' "$BASE_DIR/$SERVER_NAME/server.properties"
-      sed -i "s/motd=.*/motd=JC's $CURRENT_SERVER $SERVER_TYPE Server/g" "$BASE_DIR/$SERVER_NAME/server.properties"
+    # Set config file properties via user prompt
+    sed -i "s/eula=false/eula=true/g" "$BASE_DIR/$SERVER_NAME/eula.txt"
+    sed -i "s/difficulty=easy/difficulty=normal/g" "$BASE_DIR/$SERVER_NAME/server.properties"
+    sed -i "s/gamemode=.*/gamemode=$GAME_MODE/g" "$BASE_DIR/$SERVER_NAME/server.properties"
+    sed -i "s/pvp=true/pvp=false/g" "$BASE_DIR/$SERVER_NAME/server.properties"
+    sed -i "s/server-port=25565/server-port=$SERVER_PORT/g" "$BASE_DIR/$SERVER_NAME/server.properties"
+    sed -i "s/max-players=20/max-players=10/g" "$BASE_DIR/$SERVER_NAME/server.properties"
+    sed -i "s/motd=.*/motd=JC's $CURRENT_SERVER $SERVER_TYPE $GAME_MODE Server/g" "$BASE_DIR/$SERVER_NAME/server.properties"
 
-      # Copying in datapack
-      $SCRIPTS_DIR/all-start.sh
-      while [ ! -e "$BASE_DIR/$SERVER_NAME/world/datapacks" ]
-      do
-	sleep 2
-      done
-      $( cp ${BASE_DIR}/datapacks/* ${BASE_DIR}/${SERVER_NAME}/world/datapacks/ )
+    # Copying in datapack
+    $SCRIPTS_DIR/all-start.sh
+    while [ ! -e "$BASE_DIR/$SERVER_NAME/world/datapacks" ]
+    do
       sleep 2
-      screen -p 0 -S ${SERVER_NAME} -X eval "stuff \"reload\"\015"
-    else
-      echo "Server already configured"
-    fi
+    done
+    $( cp ${BASE_DIR}/datapacks/* ${BASE_DIR}/${SERVER_NAME}/world/datapacks/ )
+    sleep 2
+    screen -p 0 -S ${SERVER_NAME} -X eval "stuff \"reload\"\015"
+  else
+    echo "Server already configured"
   fi
 fi
--- update-server.sh
+++ update-server.sh
@@ -4,54 +4,73 @@
 #
 
 # Start by checking for the correct number of parameters
-if [ $# -lt 2 ]; then
+USAGE="Usage: $0 <server_type> <server_name> <gamemode>"
+if [ $# -lt 3 ]; then
+  echo "Invalid number of arguments given"
+  echo "$USAGE"
   echo ""
-  echo "Usage: $0 <server_type> <server_name>"
-  echo ""
+  exit 1;
+fi
+  
+# Set all script variables
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR=$( sed 's|/minecraft-scripts||g' <<< $SCRIPTS_DIR )
+SERVER_TYPE=$1
+SERVER_NAME=$2
+GAME_MODE=$3
 
+# Make sure SERVER_TYPE is valid
+if [ $SERVER_TYPE != "snapshot" || $SERVER_TYPE != "release" ]; then
+  echo "Invalid server type given"
+  echo "$USAGE"
+  echo ""
+  exit 1;
+fi
+
+# Makeu sure gamemode is valid
+if [ $GAME_MODE != "survival" || $GAME_MODE != "creative" || $GAME_MODE != "adventure" ]; then
+  echo "Inavlid gamemode"
+  echo "$USAGE"
+  echo ""
+  exit 1;
+fi
+
+if [ -e "$BASE_DIR/$SERVER_NAME/current_server" ]; then
+  CURRENT_SERVER=$( cat "$BASE_DIR/$SERVER_NAME/current_server" )
 else
-  # Set all script variables
-  SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-  BASE_DIR=$( sed 's|/minecraft-scripts||g' <<< $SCRIPTS_DIR )
-  SERVER_TYPE=$1
-  SERVER_NAME=$2
+  CURRENT_SERVER="unknown"
+fi
 
-  if [ -e "$BASE_DIR/$SERVER_NAME/current_server" ]; then
-    CURRENT_SERVER=$( cat "$BASE_DIR/$SERVER_NAME/current_server" )
-  else
-    CURRENT_SERVER="unknown"
+# If the server download detects a different version from what is running, do the following:
+#  The download script brings in the new server jar file and erases the previous one
+$SCRIPTS_DIR/get-server.sh $SERVER_TYPE $SERVER_NAME
+NEW_SERVER=$( cat "$BASE_DIR/$SERVER_NAME/current_server" )
+
+if [ ! "$CURRENT_SERVER" == "$NEW_SERVER" ]; then
+
+  # Stop the current running world
+  $SCRIPTS_DIR/stop.sh $SERVER_NAME
+
+  # Back up the current world
+  $SCRIPTS_DIR/backup-server.sh $SERVER_NAME
+
+  # Create the launch script using the latest release
+  #   Remove the current launch script first
+  if [ -e "$BASE_DIR/$SERVER_NAME/launch.sh" ]; then
+    rm "$BASE_DIR/$SERVER_NAME/launch.sh"
   fi
-
-  # If the server download detects a different version from what is running, do the following:
-  #  The download script brings in the new server jar file and erases the previous one
-  $SCRIPTS_DIR/get-server.sh $SERVER_TYPE $SERVER_NAME
-  NEW_SERVER=$( cat "$BASE_DIR/$SERVER_NAME/current_server" )
-
-  if [ ! "$CURRENT_SERVER" == "$NEW_SERVER" ]; then
-
-    # Stop the current running world
-    $SCRIPTS_DIR/stop.sh $SERVER_NAME
-
-    # Back up the current world
-    $SCRIPTS_DIR/backup-server.sh $SERVER_NAME
-
-    # Create the launch script using the latest release
-    #   Remove the current launch script first
-    if [ -e "$BASE_DIR/$SERVER_NAME/launch.sh" ]; then
-      rm "$BASE_DIR/$SERVER_NAME/launch.sh"
-    fi
     
-    # Create new launch.sh file
-    echo "#!/bin/bash" >> "$BASE_DIR/$SERVER_NAME/launch.sh"
-    echo "$BASE_DIR/java/bin/java -server -Xmx3096M -Xms3096M -jar minecraft_server.$NEW_SERVER.jar" >> "$BASE_DIR/$SERVER_NAME/launch.sh"
-    chmod +x "$BASE_DIR/$SERVER_NAME/launch.sh"
+  # Create new launch.sh file
+  echo "#!/bin/bash" >> "$BASE_DIR/$SERVER_NAME/launch.sh"
+  echo "$BASE_DIR/java/bin/java -server -Xmx3096M -Xms3096M -jar minecraft_server.$NEW_SERVER.jar" >> "$BASE_DIR/$SERVER_NAME/launch.sh"
+  chmod +x "$BASE_DIR/$SERVER_NAME/launch.sh"
 
-    # Updating the server name with version running
-    sed -i "s|motd=.*|motd=JC's $NEW_SERVER $SERVER_TYPE server|g" "$BASE_DIR/$SERVER_NAME/server.properties"
+  # Updating the server name with version running
+  sed -i "s|motd=.*|motd=JC's $NEW_SERVER $SERVER_TYPE $GAME_MODE server|g" "$BASE_DIR/$SERVER_NAME/server.properties"
+  sed -i "s|gamemode=.*|gamemode=$GAME_MODE|g" "$BASE_DIR/$SERVER_NAME/server.properties"
 
-    # Start the world back up using the new version
-    $SCRIPTS_DIR/start.sh $SERVER_NAME
-  else
-    echo "Server version has not changed. Nothing to do."
-  fi
+  # Start the world back up using the new version
+  $SCRIPTS_DIR/start.sh $SERVER_NAME
+else
+  echo "Server version has not changed. Nothing to do."
 fi
