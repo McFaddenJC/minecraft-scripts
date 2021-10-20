72521bd8cc8b5d0b2993f48ce03a0472696f2470
mcfaddenjc <mcfaddenjc@gmail.com>
2021-10-19 20:05:11 -0400

    mc inventory tracker

---
+++ show-inventory.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Server to apply this to (passed in as a variable)
+SERVER=$1
+
+# Command to list a player's inventory
+COMMAND="data get entity ${PlayerName} Inventory"
+
+# See if the screen is active and only proceed if it is
+if ! screen -list | grep -q "$SERVER"; then
+    # Server is not running so don't look for inventory
+    # echo "Server isn't running"
+    exit 0;
+else
+    # Server is running so execute commands
+
+    # Get list of the whitelisted players
+    PlayerList=$( cat /data/1/${SERVER}/whitelist.json | grep name | awk '{print $2}' | sed "s|\"||" | sed "s|\"||" )
+
+    # Loop through player list and execute the inventory command
+    for PlayerName in $PlayerList
+    do
+        screen -p 0 -S $SERVER -X eval "stuff \"${COMMAND}\"\015"
+    done
+fi
